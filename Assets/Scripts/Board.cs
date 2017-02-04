fddbd5aee87df64a721555bc4c91f844f2de9295
Grygory Dyadichenko <warsong94@gmail.com>
2017-02-04 15:52:47 +0300

    Доделал условие победы

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -21,14 +21,15 @@ public class Board
     }
     public Cell GetCell(int columnIndex, int rowIndex)
     {
-        if(columnIndex > _ColumnsCount )
+        if(columnIndex > _ColumnsCount || columnIndex < 0)
         {
             Debug.Log(string.Concat("Column index more than capacity: ", columnIndex, " > ", _ColumnsCount));
             return null;
         }
-        if(rowIndex > _RowsCount)
+        if(rowIndex > _RowsCount || rowIndex < 0)
         {
             Debug.Log(string.Concat("Row index more than capacity: ", rowIndex, " > ", _RowsCount));
+            return null;
         }
         return _Cells[columnIndex + rowIndex * _ColumnsCount];
     }
--- Assets/Scripts/BoardController.cs
+++ Assets/Scripts/BoardController.cs
@@ -6,9 +6,7 @@ using UnityEngine.UI;
 public class BoardController : Singleton<BoardController>
 {
     [SerializeField]
-    private int _RowsCount;
-    [SerializeField]
-    private int _ColumnsCount;
+    private int _BoardSize;
     [SerializeField]
     private int _MarkedCellsToWin;
     [SerializeField]
@@ -21,30 +19,33 @@ public class BoardController : Singleton<BoardController>
 
 	void Awake ()
     {
-        _Grid.constraintCount = _ColumnsCount;
+        _Grid.constraintCount = _BoardSize;
         _Grid.cellSize = new Vector2(
-            (_BoardPanel.sizeDelta.x - _Grid.padding.left * (_ColumnsCount + 1)) / _ColumnsCount,
-            (_BoardPanel.sizeDelta.y - _Grid.padding.top  * (_RowsCount + 1)) / _RowsCount);
+            (_BoardPanel.sizeDelta.x - _Grid.padding.left * (_BoardSize + 1)) / _BoardSize,
+            (_BoardPanel.sizeDelta.y - _Grid.padding.top  * (_BoardSize + 1)) / _BoardSize);
         _Board = new Board();
-        _Board.CreateCells(_CellPrefab, _RowsCount, _ColumnsCount, _Grid.transform);
+        _Board.CreateCells(_CellPrefab, _BoardSize, _BoardSize, _Grid.transform);
 	}
-
+    //Проверка условия победы
     public bool CheckWin()
     {
         bool isWin = false;
         isWin |= IsHorizontalWin();
         isWin |= IsVerticalWin();
+        isWin |= IsDiagonalWin();
         return isWin;
     }
     private bool IsHorizontalWin()
     {
-
-        for (int i = 0; i < _RowsCount; i++)
+        for (int i = 0; i < _BoardSize; i++)
         {
             int sameMarkedCount = 1;
-            for (int j = 0; j < _ColumnsCount - 1; j++)
+            for (int j = 0; j < _BoardSize - 1; j++)
             {
-                if (_Board.GetCell(j, i).PlayerIndex == _Board.GetCell(j + 1, i).PlayerIndex && _Board.GetCell(j, i).PlayerIndex != -1)
+                var firstIndex = _Board.GetCell(j, i).PlayerIndex;
+                var secondIndex = _Board.GetCell(j + 1, i).PlayerIndex;
+                if (firstIndex != -1
+                     && firstIndex == secondIndex)
                 {
                     sameMarkedCount++;
                 }
@@ -63,13 +64,15 @@ public class BoardController : Singleton<BoardController>
     }
     private bool IsVerticalWin()
     {
-        for (int j = 0; j < _ColumnsCount; j++)
+        for (int j = 0; j < _BoardSize; j++)
         {
-            for (int i = 0; i < _RowsCount - 1; i++)
+            int sameMarkedCount = 1;
+            for (int i = 0; i < _BoardSize - 1; i++)
             {
-                int sameMarkedCount = 1;
-
-                if (_Board.GetCell(j, i).PlayerIndex == _Board.GetCell(j, i + 1).PlayerIndex && _Board.GetCell(j, i).PlayerIndex != -1)
+                var firstIndex = _Board.GetCell(j, i).PlayerIndex;
+                var secondIndex = _Board.GetCell(j, i + 1).PlayerIndex;
+                if (firstIndex != -1
+                     && firstIndex == secondIndex)
                 {
                     sameMarkedCount++;
                 }
@@ -82,6 +85,112 @@ public class BoardController : Singleton<BoardController>
                 {
                     return true;
                 }
+            }
+        }
+        return false;
+    }
+    private bool IsDiagonalWin()
+    {
+        bool isWin = false;
+        isWin |= IsMainDiagonalWin();
+        isWin |= IsSubDiagonalWin();
+        return isWin;
+    }
+    private bool IsMainDiagonalWin()
+    {
+        for (int i = 0; i < _BoardSize - 1; i++)
+        {
+            int sameMarkedCount = 1;
+            for (int j = 0; j < _BoardSize - i - 1; j++)
+            {
+                var firstIndex = _Board.GetCell(j, j + i).PlayerIndex;
+                var secondIndex = _Board.GetCell(j + 1, j + i + 1).PlayerIndex;
+                if (firstIndex != -1
+                    && firstIndex == secondIndex)
+                {
+                    sameMarkedCount++;
+                }
+                else
+                {
+                    sameMarkedCount = 1;
+                }
+
+                if (sameMarkedCount == _MarkedCellsToWin)
+                {
+                    return true;
+                }
+            }
+            sameMarkedCount = 1;
+            for (int j = i; j < _BoardSize - 1; j++)
+            {
+                var firstIndex = _Board.GetCell(j, j - i).PlayerIndex;
+                var secondIndex = _Board.GetCell(j + 1, j - i + 1).PlayerIndex;
+                if (firstIndex != -1
+                    && firstIndex == secondIndex)
+                {
+                    sameMarkedCount++;
+                }
+                else
+                {
+                    sameMarkedCount = 1;
+                }
+
+                if (sameMarkedCount == _MarkedCellsToWin)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private bool IsSubDiagonalWin()
+    {
+        //Up main diagonal
+        for (int i = 0; i < _BoardSize; i++)
+        {
+            int sameMarkedCount = 1;
+            int k = i;
+            for (int j = _BoardSize - 1; j > i; j--)
+            {
+                var firstIndex = _Board.GetCell(j, k).PlayerIndex;
+                var secondIndex = _Board.GetCell(j - 1, k + 1).PlayerIndex;
+                if (firstIndex != -1
+                    && firstIndex == secondIndex)
+                {
+                    sameMarkedCount++;
+                }
+                else
+                {
+                    sameMarkedCount = 1;
+                }
+
+                if (sameMarkedCount == _MarkedCellsToWin)
+                {
+                    return true;
+                }
+                k++;
+            }
+            sameMarkedCount = 1;
+            k = 0;
+            for (int j = _BoardSize - i - 1; j > 0; j--)
+            {
+                var firstIndex = _Board.GetCell(j, k).PlayerIndex;
+                var secondIndex = _Board.GetCell(j - 1, k + 1).PlayerIndex;
+                if (firstIndex != -1
+                    && firstIndex == secondIndex)
+                {
+                    sameMarkedCount++;
+                }
+                else
+                {
+                    sameMarkedCount = 1;
+                }
+
+                if (sameMarkedCount == _MarkedCellsToWin)
+                {
+                    return true;
+                }
+                k++;
             }
         }
         return false;
